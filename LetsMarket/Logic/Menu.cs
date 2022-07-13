b97549429f16d03efc1b272bd23a9956ef696d84
Bonhin <eduardo.bonhin@gmail.com>
2022-07-13 20:51:08 -0300

    Alteração do método Execute da classe Menu

--- LetsMarket/Logic/Menu.cs
+++ LetsMarket/Logic/Menu.cs
@@ -8,30 +8,19 @@ namespace LetsMarket.Logic
     public class Menu : IMenu
     {
         public MenuItem Item { get; set; }
+        public  MenuItem SubItem { get; set; }
+
 
         public void Execute()
         {
-            Console.Clear();
-            Item = Prompt.Select(Item.title, Item.items);
-
-
-            switch (Item.Type)
-            {
-
-                case MenuType.Submenu:
-                    if (Item.items.Count == 0)
-                        Item = Prompt.Select(Item.title, Item.items);
-                    break;
-
-                case MenuType.Command:
-                    Item.action();
-                    Console.ReadKey();
-                    break;
-
-                default:
-                    break;
-            }
-            Execute();
+            SubItem = Item;
+            
+                Console.Clear();
+                SubItem = Prompt.Select(SubItem.title, SubItem.items);
+                SubItem = Prompt.Select(SubItem.title, SubItem.items);
+                SubItem.action();
+                Console.ReadKey();
+                Execute();
         }
     }
 }
