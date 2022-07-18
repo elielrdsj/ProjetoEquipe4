e3183cc02e1926100ab83eceae720e60b308883b
elielrdsj <79999508+elielrdsj@users.noreply.github.com>
2022-07-17 22:44:26 -0300

    Add files via upload

---
+++ LetsMarket/Model/Product.cs
@@ -0,0 +1,56 @@
+﻿using BetterConsoleTables;
+using LetsMarket.Db;
+using Sharprompt;
+using System.ComponentModel.DataAnnotations;
+using LetsMarket.Interfaces;
+using LetsMarket.Enums;
+
+namespace LetsMarket.Logic
+{
+    public class Product : IEntity
+    {
+        IStandardMessages _standardMessage = new StandardMessages();
+        [Display(Name = "Código")]
+        [Required(ErrorMessage = "O código é obrigatório")]
+        public string Code { get; set; }
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "A descrição é obrigatória")]
+        public string Description { get; set; }
+        [Display(Name = "Preço")]
+        [Required(ErrorMessage = "O preço é obrigatório")]
+        public decimal Price { get; set; }
+        public void Create()
+        {
+            var product = Prompt.Bind<Product>();
+            if (!_standardMessage.ShowMessageAndConfirmCreate())
+                return;
+            Database.AddProduct(product);
+            Database.Save(DatabaseOption.Products);
+        }
+        public void List()
+        {
+            _standardMessage.ListingMessage();
+            var table = new Table(TableConfiguration.UnicodeAlt());
+            table.From(Database.Products);
+            Console.WriteLine(table.ToString());
+        }
+        public void Update()
+        {
+            var produto = Prompt.Select("Selecione o Produto para Editar", Database.Products, defaultValue: Database.Products[0]);
+            Prompt.Bind(produto);
+            Database.Save(DatabaseOption.Products);
+        }
+        public void Delete()
+        {
+            var product = Prompt.Select("Selecione o Produto para Remover", Database.Products);
+            if (!_standardMessage.ShowMessageAndConfirmDelete())
+                return;
+            Database.RemoveProduct(product);
+            Database.Save(DatabaseOption.Products);
+        }
+        public override string ToString()
+        {
+            return Description;
+        }
+    }
+}
---
+++ LetsMarket/View/CreateMainMenu.cs
@@ -0,0 +1,23 @@
+﻿using LetsMarket.Enums;
+using LetsMarket.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace LetsMarket.Logic
+{
+    public static class CreateMainMenu
+    {
+        public static MenuItem Create(IEntity entity, string description)
+        {
+            var entityMenu = new MenuItem(description);
+            entityMenu.Add(new MenuItem(MenuCategory.Cadastrar + " " + description, entity.Create));
+            entityMenu.Add(new MenuItem(MenuCategory.Listar + " " + description, entity.List));
+            entityMenu.Add(new MenuItem(MenuCategory.Editar + " " + description, entity.Update));
+            entityMenu.Add(new MenuItem(MenuCategory.Remover + " " + description, entity.Delete));
+
+            return entityMenu;
+        }
+    }
+}
